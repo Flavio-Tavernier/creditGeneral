bc65072b571ee1a4eba15f98d90b92fbaa269e59
Flavio <flavio.tavernier2@gmail.com>
2025-04-04 09:20:16 +0200

    rajout comptes fixes dans H2

--- src/main/java/cgb/transfert/DatabaseInitializer.java
+++ src/main/java/cgb/transfert/DatabaseInitializer.java
@@ -23,6 +23,21 @@ public class DatabaseInitializer {
     }
 
     public static void insertSampleData(AccountRepository accountRepository) {
+        Account account1 = new Account();
+        account1.setAccountNumber("123456789");
+        account1.setSolde(300.00);
+        accountRepository.save(account1);
+
+        Account account2 = new Account();
+        account2.setAccountNumber("987654321");
+        account2.setSolde(500.00);
+        accountRepository.save(account2);
+
+        Account account3 = new Account();
+        account3.setAccountNumber("456789123");
+        account3.setSolde(2000.00);
+        accountRepository.save(account3);
+        
         for (int i = 0; i < 20; i++) {
             Account account = new Account();
         	account.setAccountNumber(generateRandomIban());	
